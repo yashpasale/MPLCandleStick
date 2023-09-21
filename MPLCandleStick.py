9c02030a588d17adf325af66855bcd8562b99541
yashpasale <116392993+yashpasale@users.noreply.github.com>
2023-09-21 08:20:34 -0400

    Add files via upload

---
+++ MPLCandleStick.py
@@ -0,0 +1,17 @@
+#%%
+import pandas as pd
+import mplfinance as mpf
+#%%
+df = 'NVDA.csv'
+data = pd.read_csv(r"/Users/yashpasale/Downloads/NVDA.csv")
+print(df)
+# %%
+data.Date = pd.to_datetime(data.Date)
+data.info()
+# %%
+data = data.set_index('Date')
+
+# %%
+mpf.plot(data['2022-12': '2023-05'],type='candle',mav=(50) ,volume=True, figratio=(20,12), title='NVDA YTD', tight_layout=True)
+
+# %%
